59ab25c15fd9addf293166f1753077a897a29c91
YouHyunKim <yuhyunk7@gmail.com>
2023-03-31 11:45:26 +0900

    setting

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView.builder(
+        itemCount: 5,
+        itemBuilder: (context, index) {
+        return Image.network("https://picsum.photos/${index+1}/237/200/300");
+      },),
+    );
+  }
+}
+
+
