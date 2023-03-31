3ce36a39e4f4c01d2191e9937fe22f80ff81d4a5
YouHyunKim <yuhyunk7@gmail.com>
2023-03-31 12:31:16 +0900

    appbar에도 body에 있는 사진이 보이게 stack사용

--- lib/main.dart
+++ lib/main.dart
@@ -22,15 +22,39 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView.builder(
-      itemCount: 5,
-      itemBuilder: (context, index) {
-        return Image.network(
-          "https://picsum.photos/id/${index + 1}/200/300",
-          fit: BoxFit.cover,
-        );
-      },
-    ),
+      body: Stack(
+        children: [
+          Image.network(
+            "https://picsum.photos/400/200",
+            fit: BoxFit.cover,
+            height: kToolbarHeight + MediaQuery.of(context).padding.top,
+          ),
+          PageView.builder(
+            itemCount: 10,
+            itemBuilder: (context, index) {
+              return Image.network(
+                "https://picsum.photos/id/${index + 1}/200/300",
+                fit: BoxFit.cover,
+              );
+            },
+          ),
+          Positioned(
+            top: MediaQuery.of(context).padding.top,
+            left: 0,
+            right: 0,
+            child: AppBar(
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              title: const Text(
+                'Around',
+                style: TextStyle(
+                  fontSize: 50,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
